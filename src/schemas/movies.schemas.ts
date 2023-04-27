0572d75f7109c43c18a90189c358aedaa26f3082
Renato Gouveia <rpgouveia@gmail.com>
2023-04-27 14:34:11 -0300

    fix: update movieSchema for createMovie functionality

--- src/schemas/movies.schemas.ts
+++ src/schemas/movies.schemas.ts
@@ -2,10 +2,10 @@ import { z } from "zod"
 
 const movieSchema = z.object({
   id: z.number(),
-  name: z.string(),
+  name: z.string().max(50),
   description: z.string().nullish(),
-  duration: z.number(),
-  price: z.number()
+  duration: z.number().positive(),
+  price: z.number().int()
 })
 const moviesListSchema = z.array(movieSchema)
 const movieSchemaRequest = movieSchema.omit({ id: true })
